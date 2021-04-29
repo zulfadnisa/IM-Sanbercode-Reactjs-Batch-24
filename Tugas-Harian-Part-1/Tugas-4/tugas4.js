0da386549b02c244c953615d1cb549d6f3135c00
zulfadnisa <zulfadza@gmail.com>
2021-04-30 05:33:07 +0700

    update task4

--- Tugas-Harian-Part-1/Tugas-4/tugas4.js
+++ Tugas-Harian-Part-1/Tugas-4/tugas4.js
@@ -1,28 +1,28 @@
 // soal 1
-console.log('LOOPING PERTAMA')
+console.log('LOOPING PERTAMA');
 var i = 2;
 while (i <= 20 && i % 2 == 0) {
-    console.log(i + ' - I love coding')
-    i = i + 2
+    console.log(i + ' - I love coding');
+    i = i + 2;
 }
-console.log('LOOPING KEDUA')
-var j = 20
+console.log('LOOPING KEDUA');
+var j = 20;
 while (j >= 2 && j % 2 == 0) {
-    console.log(j + ' - I will become a frontend developer')
-    j = j - 2
+    console.log(j + ' - I will become a frontend developer');
+    j = j - 2;
 }
 
 // soal 2
 for (var k = 1; k <= 20; k++) {
     if (k % 2 == 0) {
-        console.log(k + ' - Berkualitas')
+        console.log(k + ' - Berkualitas');
     }
     else {
         if (k % 3 == 0) {
-            console.log(k + ' - I Love Coding')
+            console.log(k + ' - I Love Coding');
         }
         else {
-            console.log(k + ' - Santai')
+            console.log(k + ' - Santai');
         }
     }
 }
@@ -31,25 +31,25 @@ for (var k = 1; k <= 20; k++) {
 var jumlah = '';
 for (var i = 1; i < 8; i++) {
     jumlah = jumlah + '#';
-    console.log(jumlah)
+    console.log(jumlah);
 }
 
 // soall 4
-var kalimat=["aku", "saya", "sangat", "sangat", "senang", "belajar", "javascript"]
-kalimat.shift()
-kalimat.splice(1,1)
-console.log(kalimat.join(' '))
+var kalimat = ["aku", "saya", "sangat", "sangat", "senang", "belajar", "javascript"];
+kalimat.shift();
+kalimat.splice(1, 1);
+console.log(kalimat.join(' '));
 
 // soal 5
-var sayuran=[]
-sayuran.push('Kangkung')
-sayuran.push('Bayam')
-sayuran.push('Buncis')
-sayuran.push('Kubis')
-sayuran.push('Timun')
-sayuran.push('Seledri')
-sayuran.push('Tauge')
-sayuran.sort()
-for(var i=1; i<8; i++){
-    console.log(i+'.'+sayuran[i-1])
+var sayuran = [];
+sayuran.push('Kangkung');
+sayuran.push('Bayam');
+sayuran.push('Buncis');
+sayuran.push('Kubis');
+sayuran.push('Timun');
+sayuran.push('Seledri');
+sayuran.push('Tauge');
+sayuran.sort();
+for (var i = 1; i < 8; i++) {
+    console.log(i + '.' + sayuran[i - 1]);
 }
