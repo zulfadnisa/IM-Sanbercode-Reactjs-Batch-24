fe1a078f6f1d2b3bac5a838d9a775b3be693fba7
zulfadnisa <zulfadza@gmail.com>
2021-05-21 01:11:15 +0700

    delete comment

--- Tugas-Harian-Part-2/my-first-react/src/Tasks/Tugas-14/Context/StudentsContext.js
+++ Tugas-Harian-Part-2/my-first-react/src/Tasks/Tugas-14/Context/StudentsContext.js
@@ -2,7 +2,6 @@ import React from "react";
 import axios from "axios";
 
 export const StudentsContext = React.createContext();
-export const StudentEditContext = React.createContext();
 export const StudentsProvider = (props) => {
   const [students, setStudents] = React.useState([]);
   React.useEffect(() => {
@@ -25,24 +24,6 @@ export const StudentsProvider = (props) => {
     };
     fetchData();
   }, [props]);
-
-  // const toScoreIndex = (score) => {
-  //   let index;
-  //   if (score >= 80) {
-  //     index = "A";
-  //   } else if (score >= 50 && score < 80) {
-  //     if (score < 60) {
-  //       index = "D";
-  //     } else if (score < 70) {
-  //       index = "C";
-  //     } else {
-  //       index = "B";
-  //     }
-  //   } else {
-  //     index = "E";
-  //   }
-  //   return index;
-  // };
   return (
     <StudentsContext.Provider value={[students, setStudents]}>
       {props.children}
